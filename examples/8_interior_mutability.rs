cf13b2b077183a5a6d235a7fd60e419ce27d5994
kylinkzhang <kylinkzhang@tencent.com>
2021-09-30 19:34:11 +0800

    temp save

--- examples/8_interior_mutability.rs
+++ examples/8_interior_mutability.rs
@@ -2,6 +2,17 @@ pub trait Messenger {
     fn send(&self, msg: &str);
 }
 
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T> where T: Messenger {
+
+
+
+}
 
 
 fn main() {}
