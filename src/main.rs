40e88bef2c42999053eb791cc555d269a4d4198e
JasonkayZK <271226192@qq.com>
2021-10-03 23:55:46 +0800

    fix: ci err

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use my_libs::hello::say_hello;
 fn main() {
     match say_hello("jasonkay") {
         Ok(res) => {
-            if res { println!("let's rust-up!") }
+            if res {
+                println!("let's rust-up!")
+            }
         }
-        Err(e) => { println!("error occurred: {:?}", e) }
+        Err(e) => {
+            println!("error occurred: {:?}", e)
+        }
     }
 }
