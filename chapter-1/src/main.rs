f7229b2746c5d5162d85a8427421df9cd6198e0f
JasonkayZK <271226192@qq.com>
2022-01-07 20:32:36 +0800

    finished: chapter 1

---
+++ chapter-1/src/main.rs
@@ -0,0 +1,46 @@
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", *region);
+    }
+}
+
+fn harder() {
+    let penguin_data = "\
+   common name,length (cm)
+   Little penguin,33
+   Yellow-eyed penguin,65
+   Fiordland penguin,60
+   Invalid,data
+   ";
+
+    let records = penguin_data.lines();
+
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            // Empty line
+            continue;
+        }
+
+        let fields: Vec<_> = record.split(",").map(|field| field.trim()).collect();
+        if cfg!(debug_assertions) {
+            // 输出到标准错误输出
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() {
+            // 输出到标准输出
+            println!("{}, {}cm", name, length);
+        }
+    }
+}
+
+fn main() {
+    greet_world();
+
+    harder();
+}
