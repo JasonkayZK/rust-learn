54958b36ce9678eeb03c5936f51b15f0d3d0c796
JasonkayZK <271226192@qq.com>
2022-01-17 18:28:57 +0800

    update readme

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-use my_libs::hello::say_hello;
-
-fn main() {
-    match say_hello("jasonkay") {
-        Ok(res) => {
-            if res {
-                println!("let's rust-up!")
-            }
-        }
-        Err(e) => {
-            println!("error occurred: {:?}", e)
-        }
-    }
-}
+fn main() {}
