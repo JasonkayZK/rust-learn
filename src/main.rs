eed90bbdb1d11ab465bc25e8cfa2bcbfd06148f3
JasonkayZK <271226192@qq.com>
2022-01-18 15:55:56 +0800

    temp save

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use libc::size_t;
+
+#[link(name = "snappy")]
+extern "C" {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+}
+
+fn main() {
+    let x = unsafe { snappy_max_compressed_length(100) };
+    println!("max compressed length of a 100 byte buffer: {}", x);
+}
