7cd0b87f922d09bb0888b118251f2cbbff257fb9
JasonkayZK <271226192@qq.com>
2022-02-02 21:09:24 +0800

    init async

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use my_libs::hello::say_hello;
+use futures::executor::block_on;
+
+async fn async_demo1() -> i32 {
+    println!("55");
+    55
+}
+
+async fn async_demo2() -> i32 {
+    println!("44");
+    44
+}
+
+async fn async_demo3() -> i32 {
+    println!("1");
+    1
+}
+
+async fn call_async() {
+    let (r1, r2, r3) = futures::join!(async_demo1(), async_demo2(), async_demo3());
+
+    println!("res: {:?}", r1 + r2 + r3)
+}
 
 fn main() {
-    match say_hello("jasonkay") {
-        Ok(res) => {
-            if res {
-                println!("let's rust-up!")
-            }
-        }
-        Err(e) => {
-            println!("error occurred: {:?}", e)
-        }
-    }
+    block_on(call_async());
 }
