dc055203f4e076026d22a32146085b2da53d99fe
JasonkayZK <271226192@qq.com>
2022-11-05 10:36:42 +0800

    temp save

---
+++ examples/22_futures.rs
@@ -0,0 +1,18 @@
+mod connection;
+
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    let what_is_this = my_async_fn();
+    // Nothing has been printed yet.
+    what_is_this.await;
+    // Text has been printed and socket has been
+    // established and closed.
+}
+
+async fn my_async_fn() {
+    println!("hello from async");
+    let _socket = TcpStream::connect("127.0.0.1:3000").await.unwrap();
+    println!("async TCP operation complete");
+}
---
+++ examples/25_waker.rs
@@ -0,0 +1,82 @@
+extern crate core;
+
+use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread;
+use std::time::{Duration, Instant};
+
+use futures::task;
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // Get a handle to the waker for the current task
+            let waker = cx.waker().clone();
+            let when = self.when;
+            // Spawn a timer thread.
+            thread::spawn(move || {
+                let now = Instant::now();
+                if now < when {
+                    thread::sleep(when - now);
+                }
+                waker.wake();
+            });
+
+            Poll::Pending
+        }
+    }
+}
+
+fn main() {
+    let mut mini_tokio = MiniTokio::new();
+
+    for i in 0..10 {
+        mini_tokio.spawn(async move {
+            let when = Instant::now() + Duration::from_millis(i * 1000);
+            let future = Delay { when };
+
+            let out = future.await;
+            assert_eq!(out, "done");
+        });
+        mini_tokio.run();
+    }
+}
+
+struct MiniTokio {
+    tasks: VecDeque<Task>,
+}
+
+type Task = Pin<Box<dyn Future<Output=()> + Send>>;
+
+impl MiniTokio {
+    pub fn new() -> Self {
+        Self { tasks: VecDeque::new() }
+    }
+
+    fn spawn<F>(&mut self, future: F)
+        where F: Future<Output=()> + Send + 'static {
+        self.tasks.push_back(Box::pin(future));
+    }
+
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+
+        while let Some(mut task) = self.tasks.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.tasks.push_back(task);
+            }
+        }
+    }
+}
