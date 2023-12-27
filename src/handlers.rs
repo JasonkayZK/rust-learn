6dce5e2a4e2c8248bdfe3d1b3483d05bce1b82d4
jasonkayzk <271226192@qq.com>
2023-12-27 21:47:20 +0800

    refactor, finished test

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,13 +1,17 @@
 use std::collections::HashSet;
 
 use anyhow::Result;
+use libp2p::floodsub::FloodsubEvent;
+use libp2p::futures::StreamExt;
+use libp2p::mdns::Event;
+use libp2p::swarm::SwarmEvent;
 use libp2p::Swarm;
-use log::{error, info};
+use log::{debug, error, info};
 use tokio::fs;
 use tokio::sync::mpsc;
 
-use crate::behaviour::RecipeBehaviour;
-use crate::consts::{STORAGE_FILE_PATH, TOPIC};
+use crate::behaviour::{RecipeBehaviour, RecipeBehaviourEvent};
+use crate::consts::{PEER_ID, STORAGE_FILE_PATH, TOPIC};
 use crate::models::{ListMode, ListRequest, ListResponse, Recipe};
 
 pub async fn handle_list_peers(swarm: &mut Swarm<RecipeBehaviour>) {
@@ -87,6 +91,92 @@ pub async fn handle_list_recipes(cmd: &str, swarm: &mut Swarm<RecipeBehaviour>) 
     };
 }
 
+pub async fn handle_swarm_event(
+    response_sender: mpsc::UnboundedSender<ListResponse>,
+    swarm: &mut Swarm<RecipeBehaviour>,
+) {
+    let event = swarm.select_next_some().await;
+    info!("Income swarm Event: {:?}", event);
+
+    match event {
+        SwarmEvent::Behaviour(recipe_behaviours) => match recipe_behaviours {
+            RecipeBehaviourEvent::Floodsub(flood_sub_event) => match flood_sub_event {
+                FloodsubEvent::Message(msg) => {
+                    if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
+                        if resp.receiver == PEER_ID.to_string() {
+                            info!("Response from {}:", msg.source);
+                            resp.data.iter().for_each(|r| info!("{:?}", r));
+                        }
+                    } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
+                        match req.mode {
+                            ListMode::All => {
+                                info!("Received ALL req: {:?} from {:?}", req, msg.source);
+                                respond_with_public_recipes(
+                                    response_sender.clone(),
+                                    msg.source.to_string(),
+                                );
+                            }
+                            ListMode::One(ref peer_id) => {
+                                if peer_id == &PEER_ID.to_string() {
+                                    info!("Received req: {:?} from {:?}", req, msg.source);
+                                    respond_with_public_recipes(
+                                        response_sender.clone(),
+                                        msg.source.to_string(),
+                                    );
+                                }
+                            }
+                        }
+                    }
+                }
+                FloodsubEvent::Subscribed { .. } => {}
+                FloodsubEvent::Unsubscribed { .. } => {}
+            },
+            RecipeBehaviourEvent::Mdns(mdns_event) => match mdns_event {
+                Event::Discovered(discovered_list) => {
+                    let behavior_mut = swarm.behaviour_mut();
+                    for (peer, _addr) in discovered_list {
+                        behavior_mut.flood_sub.add_node_to_partial_view(peer);
+                    }
+                }
+                Event::Expired(expired_list) => {
+                    let behavior_mut = swarm.behaviour_mut();
+                    for (peer, _addr) in expired_list {
+                        if !behavior_mut.mdns.has_node(&peer) {
+                            behavior_mut.flood_sub.remove_node_from_partial_view(&peer);
+                        }
+                    }
+                }
+            },
+        },
+        SwarmEvent::ConnectionEstablished {
+            peer_id,
+            connection_id,
+            endpoint,
+            num_established,
+            ..
+        } => {
+            debug!("[Connection established] peer_id: {}, connection_id: {}, endpoint: {:?}, num_established: {:?}", peer_id, connection_id, endpoint, num_established);
+        }
+        SwarmEvent::ConnectionClosed {
+            peer_id,
+            connection_id,
+            endpoint,
+            num_established,
+            ..
+        } => {
+            debug!("[Connection closed] peer_id: {}, connection_id: {}, endpoint: {:?}, num_established: {:?}", peer_id, connection_id, endpoint, num_established);
+        }
+        SwarmEvent::IncomingConnection { .. } => {}
+        SwarmEvent::IncomingConnectionError { .. } => {}
+        SwarmEvent::OutgoingConnectionError { .. } => {}
+        SwarmEvent::NewListenAddr { .. } => {}
+        SwarmEvent::ExpiredListenAddr { .. } => {}
+        SwarmEvent::ListenerClosed { .. } => {}
+        SwarmEvent::ListenerError { .. } => {}
+        SwarmEvent::Dialing { .. } => {}
+    };
+}
+
 async fn publish_recipe(id: usize) -> Result<()> {
     let mut local_recipes = read_local_recipes().await?;
     local_recipes
@@ -132,7 +222,7 @@ async fn read_local_recipes() -> Result<Vec<Recipe>> {
     Ok(result)
 }
 
-pub fn respond_with_public_recipes(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
+fn respond_with_public_recipes(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
     tokio::spawn(async move {
         match read_local_recipes().await {
             Ok(recipes) => {
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,19 @@ use std::env;
 use std::error::Error;
 use std::time::Duration;
 
-use libp2p::{mdns, noise, Swarm, tcp, yamux};
-use libp2p::floodsub::{Floodsub, FloodsubEvent};
-use libp2p::futures::StreamExt;
-use libp2p::mdns::Event;
-use libp2p::swarm::SwarmEvent;
+use libp2p::floodsub::Floodsub;
+use libp2p::{mdns, noise, tcp, yamux, Swarm};
 use log::{error, info};
 use tokio::io::AsyncBufReadExt;
 use tokio::sync::mpsc;
 
-use crate::behaviour::{RecipeBehaviour, RecipeBehaviourEvent};
+use crate::behaviour::RecipeBehaviour;
 use crate::consts::{KEYS, PEER_ID, TOPIC};
 use crate::handlers::{
     handle_create_recipe, handle_list_peers, handle_list_recipes, handle_publish_recipe,
-    respond_with_public_recipes,
+    handle_swarm_event,
 };
-use crate::models::{EventType, ListMode, ListRequest, ListResponse};
+use crate::models::EventType;
 
 mod behaviour;
 mod consts;
@@ -26,7 +23,7 @@ mod models;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    env::set_var("RUST_LOG", "debug");
+    env::set_var("RUST_LOG", "info");
     pretty_env_logger::init();
 
     info!("Peer Id: {}", PEER_ID.clone());
@@ -52,7 +49,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .parse()
             .expect("can get a local socket"),
     )
-        .expect("swarm can be started");
+    .expect("swarm can be started");
 
     swarm.behaviour_mut().flood_sub.subscribe(TOPIC.clone());
 
@@ -85,78 +82,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-}
-
-async fn handle_swarm_event(
-    response_sender: mpsc::UnboundedSender<ListResponse>,
-    swarm: &mut Swarm<RecipeBehaviour>,
-) {
-    let event = swarm.select_next_some().await;
-    info!("Income swarm Event: {:?}", event);
-
-    match event {
-        SwarmEvent::Behaviour(recipe_behaviours) => match recipe_behaviours {
-            RecipeBehaviourEvent::Floodsub(flood_sub_event) => match flood_sub_event {
-                FloodsubEvent::Message(msg) => {
-                    if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
-                        if resp.receiver == PEER_ID.to_string() {
-                            info!("Response from {}:", msg.source);
-                            resp.data.iter().for_each(|r| info!("{:?}", r));
-                        }
-                    } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
-                        match req.mode {
-                            ListMode::All => {
-                                info!("Received ALL req: {:?} from {:?}", req, msg.source);
-                                respond_with_public_recipes(
-                                    response_sender.clone(),
-                                    msg.source.to_string(),
-                                );
-                            }
-                            ListMode::One(ref peer_id) => {
-                                if peer_id == &PEER_ID.to_string() {
-                                    info!("Received req: {:?} from {:?}", req, msg.source);
-                                    respond_with_public_recipes(
-                                        response_sender.clone(),
-                                        msg.source.to_string(),
-                                    );
-                                }
-                            }
-                        }
-                    }
-                }
-                FloodsubEvent::Subscribed { .. } => {}
-                FloodsubEvent::Unsubscribed { .. } => {}
-            },
-            RecipeBehaviourEvent::Mdns(mdns_event) => match mdns_event {
-                Event::Discovered(discovered_list) => {
-                    let behavior_mut = swarm.behaviour_mut();
-                    for (peer, _addr) in discovered_list {
-                        behavior_mut.flood_sub.add_node_to_partial_view(peer);
-                    }
-                }
-                Event::Expired(expired_list) => {
-                    let behavior_mut = swarm.behaviour_mut();
-                    for (peer, _addr) in expired_list {
-                        if !behavior_mut.mdns.has_node(&peer) {
-                            behavior_mut.flood_sub.remove_node_from_partial_view(&peer);
-                        }
-                    }
-                }
-            },
-        },
-        SwarmEvent::ConnectionEstablished { peer_id, connection_id, endpoint, num_established, .. } => {
-            info!("[Connection established] peer_id: {}, connection_id: {}, endpoint: {:?}, num_established: {:?}", peer_id, connection_id, endpoint, num_established);
-        }
-        SwarmEvent::ConnectionClosed { peer_id, connection_id, endpoint, num_established, .. } => {
-            info!("[Connection closed] peer_id: {}, connection_id: {}, endpoint: {:?}, num_established: {:?}", peer_id, connection_id, endpoint, num_established);
-        }
-        SwarmEvent::IncomingConnection { .. } => {}
-        SwarmEvent::IncomingConnectionError { .. } => {}
-        SwarmEvent::OutgoingConnectionError { .. } => {}
-        SwarmEvent::NewListenAddr { .. } => {}
-        SwarmEvent::ExpiredListenAddr { .. } => {}
-        SwarmEvent::ListenerClosed { .. } => {}
-        SwarmEvent::ListenerError { .. } => {}
-        SwarmEvent::Dialing { .. } => {}
-    };
 }
